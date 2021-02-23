b531b2965639d60dacdeea525856d512a2828147
David Steinsland <david@steinsland.email>
2021-02-23 12:15:57 +0100

    bumper dependencies

--- src/main/kotlin/no/nav/helse/sparkel/dkif/Dkifløser.kt
+++ src/main/kotlin/no/nav/helse/sparkel/dkif/Dkifløser.kt
@@ -3,6 +3,7 @@ package no.nav.helse.sparkel.dkif
 import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import org.slf4j.LoggerFactory
@@ -27,11 +28,11 @@ internal class Dkifløser(
         }.register(this)
     }
 
-    override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
+    override fun onError(problems: MessageProblems, context: MessageContext) {
         sikkerlogg.error("forstod ikke $behov med melding\n${problems.toExtendedReport()}")
     }
 
-    override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
         sikkerlogg.info("mottok melding: ${packet.toJson()}")
         dkifService.løsningForBehov(
             packet["@id"].asText(),
@@ -41,7 +42,7 @@ internal class Dkifløser(
                 behov to mapOf("erDigital" to (løsning?.takeUnless { it.isMissingNode }?.let { DigitalKontaktinformasjon(it).erDigital }
                     ?: false))
             )
-            context.send(packet.toJson().also { json ->
+            context.publish(packet.toJson().also { json ->
                 sikkerlogg.info(
                     "sender svar {} for {}:\n\t{}",
                     keyValue("id", packet["@id"].asText()),
--- src/test/kotlin/no/nav/helse/sparkel/dkif/DkifløserTest.kt
+++ src/test/kotlin/no/nav/helse/sparkel/dkif/DkifløserTest.kt
@@ -23,23 +23,19 @@ internal class DkifløserTest {
     private lateinit var sendtMelding: JsonNode
     private lateinit var service: DkifService
 
-    private val context = object : RapidsConnection.MessageContext {
-        override fun send(message: String) {
-            sendtMelding = objectMapper.readTree(message)
-        }
-
-        override fun send(key: String, message: String) {}
-    }
-
     private val rapid = object : RapidsConnection() {
 
         fun sendTestMessage(message: String) {
-            listeners.forEach { it.onMessage(message, context) }
+            listeners.forEach { it.onMessage(message, this) }
         }
 
-        override fun publish(message: String) {}
+        override fun publish(message: String) {
+            sendtMelding = objectMapper.readTree(message)
+        }
 
-        override fun publish(key: String, message: String) {}
+        override fun publish(key: String, message: String) {
+            sendtMelding = objectMapper.readTree(message)
+        }
 
         override fun start() {}
 
